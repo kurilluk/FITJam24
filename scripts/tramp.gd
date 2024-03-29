9bafe4a1069e44cfbda740a544a27cf4ba2d0c8f
Lukas Kurilla <kurilluk@email.cz>
2024-03-29 16:59:55 +0100

    Added player step logic

--- scripts/tramp.gd
+++ scripts/tramp.gd
@@ -5,8 +5,9 @@ enum State { IDLE, FOLLOW }
 const MASS = 10.0
 const ARRIVE_DISTANCE = 10.0
 
-@export var speed: float = 200.0
+@export var speed: float = 100.0
 @export var steps: int = 3
+@export var goal: Vector2 = Vector2.ZERO
 
 var _state = State.IDLE
 var _velocity = Vector2()
@@ -16,11 +17,13 @@ var _velocity = Vector2()
 var _click_position = Vector2()
 var _path = PackedVector2Array()
 var _next_point = Vector2()
-var _steps
+#var _steps
 
 func _ready():
 	_change_state(State.IDLE)
-	_steps = steps
+	#_steps = steps
+	_click_position = goal
+	_change_state(State.FOLLOW)
 
 
 func _process(_delta):
@@ -29,23 +32,23 @@ func _process(_delta):
 	var arrived_to_next_point = _move_to(_next_point)
 	if arrived_to_next_point:
 		_path.remove_at(0)
-		_steps -= 1
-		if _steps <= 0:
-			_change_state(State.IDLE)
+		#_steps -= 1
+		#if _steps <= 0:
+			#_change_state(State.IDLE)
 		if _path.is_empty():
 			_change_state(State.IDLE)
 			return
 		_next_point = _path[0]
 
 
-func _unhandled_input(event):
-	_click_position = get_global_mouse_position()
-	if _tile_map.is_point_walkable(_click_position):
-		#if event.is_action_pressed(&"teleport_to", false, true):
-			#_change_state(State.IDLE)
-			#global_position = _tile_map.round_local_position(_click_position)
-		if event.is_action_pressed(&"move_to"):
-			_change_state(State.FOLLOW)
+#func _unhandled_input(event):
+	#_click_position = get_global_mouse_position()
+	#if _tile_map.is_point_walkable(_click_position):
+		##if event.is_action_pressed(&"teleport_to", false, true):
+			##_change_state(State.IDLE)
+			##global_position = _tile_map.round_local_position(_click_position)
+		#if event.is_action_pressed(&"move_to"):
+			#_change_state(State.FOLLOW)
 
 
 func _move_to(local_position):
@@ -60,7 +63,7 @@ func _move_to(local_position):
 func _change_state(new_state):
 	if new_state == State.IDLE:
 		_tile_map.clear_path()
-		_steps = steps
+		#_steps = steps
 	elif new_state == State.FOLLOW:
 		_path = _tile_map.find_path(position, _click_position)
 		if _path.size() < 2:
