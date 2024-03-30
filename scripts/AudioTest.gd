72b9422695fb4bffbaa94c57d10a99b859f973b9
Lukas Kurilla <kurilluk@email.cz>
2024-03-30 17:26:29 +0100

    Audio script update

--- scripts/AudioTest.gd
+++ scripts/AudioTest.gd
@@ -15,15 +15,17 @@ extends AudioStreamPlayer
 	set(value):
 		AudioServer.get_bus_effect(1,0).tap1_delay_ms = value
 		
-var LowPass:int:
+@export var LowPass:int:
 	get:
 		return AudioServer.get_bus_effect(2,0).cutoff_hz
 	set(value):
 		AudioServer.get_bus_effect(2,0).cutoff_hz = value
 
+@export var volume_dis : float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	LowPass = 20000
+	AudioServer.set_bus_volume_db(1,volume_dis)
 	#pass # Replace with function body.
 
 
@@ -31,4 +33,9 @@ func _ready():
 func _process(delta):
 	LowPass -= 1000 * delta
 	print("filter",LowPass)
+	AudioServer.get_bus_volume_db(1)
+	volume_dis -= 0.01
+	print("volume",volume_dis)
+	AudioServer.set_bus_volume_db(1,volume_dis)
+
 	#pass
